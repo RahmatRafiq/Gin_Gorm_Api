35354836eedb6e11c86cbf7a2cdde0e04f6efb82
RahmatRafiq <rahmatrafiq.1999@gmail.com>
2024-03-19 06:53:11 +0800

    Add UserResponses struct to UserController.go

--- controllers/UserController/UserController.go
+++ controllers/UserController/UserController.go
@@ -3,6 +3,7 @@ package UserController
 import (
 	"Gin_Gorm_Api/database"
 	models "Gin_Gorm_Api/models/UsersModels"
+	"Gin_Gorm_Api/responses"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -27,15 +28,21 @@ func GetAllUser(ctx *gin.Context) {
 
 func GetUser(ctx *gin.Context) {
 	id := ctx.Param("id")
-	user := new(models.Users)
+	user := new(responses.UserResponses)
 	errDb := database.DB.Table("users").Where("id = ?", id).First(&user).Error
 	if errDb != nil || user.ID == nil {
-		ctx.JSON(http.StatusNotFound, gin.H{
-			"message": "Data Not Found",
+		ctx.JSON(500, gin.H{
+			"message": "Internal Server Error",
 		})
 		return
 	}
 
+	if user.ID == nil {
+		ctx.JSON(http.StatusNotFound, gin.H{
+			"message": "Internal Server Error",
+		})
+		return
+	}
 	ctx.JSON(200, gin.H{
 		"message": "Data Found",
 		"data":    user,
