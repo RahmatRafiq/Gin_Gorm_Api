58aaddad7c06683ff4b5f9adcef64118cb598fe6
RahmatRafiq <rahmatrafiq.1999@gmail.com>
2024-03-19 06:45:44 +0800

    Update UserResponses struct fields to use pointer types

--- responses/UsersResponse.go
+++ responses/UsersResponse.go
@@ -1,9 +1,9 @@
 package responses
 
 type UserResponses struct {
-	ID          uint   `json:"id"`
-	Username    string `json:"username"`
-	Password    string `json:"password"`
-	Role        string `json:"role"`
-	MahasiswaID uint   `json:"id_mahasiswa"`
+	ID          *uint   `json:"id"`
+	Username    *string `json:"username"`
+	Password    *string `json:"password"`
+	Role        *string `json:"role"`
+	MahasiswaID *uint   `json:"id_mahasiswa"`
 }
