2a17d7fc0fdc2142483c4a9264ac62177588b68c
RahmatRafiq <rahmatrafiq.1999@gmail.com>
2024-03-18 19:10:35 +0800

    Add godotenv package for environment variable management

--- bootstrap/index.go
+++ bootstrap/index.go
@@ -3,11 +3,19 @@ package bootstrap
 import (
 	"Gin_Gorm_Api/database"
 	"Gin_Gorm_Api/routes"
+	"log"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func BootstrapApp() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Error loading .env file")
+
+	}
+
 	database.ConnectDatabase()
 	app := gin.Default()
 
