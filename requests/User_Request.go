7e17532fbd41504534df6d4036a82c692da92ed4
RahmatRafiq <rahmatrafiq.1999@gmail.com>
2024-03-20 02:42:17 +0800

    Update UserRequest struct field tags using form tag

--- requests/User_Request.go
+++ requests/User_Request.go
@@ -1,9 +1,9 @@
 package requests
 
 type UserRequest struct {
-	Username    string `json:"username" binding:"required"`
-	Password    string `json:"password" binding:"required"`
-	Role        string `json:"role" binding:"required"`
-	MahasiswaID int    `json:"id_mahasiswa" binding:"required"`
+	Username    string `json:"username" form:"name" binding:"required"`
+	Password    string `json:"password" form:"password" binding:"required"`
+	Role        string `json:"role" form:"role" binding:"required"`
+	MahasiswaID int    `json:"id_mahasiswa" form:"id_mahasiswa" binding:"required"`
 	// Email    string `json:"email" binding:"required,email,unique"`
 }
