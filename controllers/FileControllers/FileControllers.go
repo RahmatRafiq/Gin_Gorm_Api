f5237c9d0700efcf074e251043522eaed72b5e90
RahmatRafiq <rahmatrafiq.1999@gmail.com>
2024-03-21 05:44:18 +0800

    Add file upload functionality to FileControllers and update route for file upload

--- controllers/FileControllers/FileControllers.go
+++ controllers/FileControllers/FileControllers.go
@@ -1,8 +1,38 @@
 package FileControllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
 
 func UploadFile(ctx *gin.Context) {
+
+	fileHeader, _ := ctx.FormFile("file")
+	if fileHeader == nil {
+		ctx.AbortWithStatusJSON(400, gin.H{
+			"message": "File is required",
+		})
+		return
+	}
+
+	// file, errFile := fileHeader.Open()
+	// 	if errFile != nil {
+	// 		panic(errFile)
+	// 	}
+
+	// extensionFile := filepath.Ext(fileHeader.Filename)
+	// filename :=
+
+	errUpload := ctx.SaveUploadedFile(fileHeader, fmt.Sprintf("./public/files/%s", fileHeader.Filename))
+
+	if errUpload != nil {
+		ctx.JSON(500, gin.H{
+			"message": "Internal Server Error, Failed to upload file",
+		})
+		return
+	}
+
 	ctx.JSON(200, gin.H{
 		"message": "File Uploaded",
 	})
