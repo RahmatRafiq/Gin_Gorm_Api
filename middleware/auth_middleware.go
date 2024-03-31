e8c3aa2933914a6e54a895e43961d1c185ecde53
RahmatRafiq <rahmatrafiq.1999@gmail.com>
2024-04-01 04:02:04 +0800

    Add authentication middleware and token validation on auth_middleware

--- middleware/auth_middleware.go
+++ middleware/auth_middleware.go
@@ -1,8 +1,40 @@
 package middleware
 
-import "github.com/gin-gonic/gin"
+import (
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
 
 func AuthMiddleware(ctx *gin.Context) {
+	bearerToken := ctx.GetHeader("Authorization")
+
+	if !strings.Contains(bearerToken, "Bearer") {
+		ctx.AbortWithStatusJSON(401, gin.H{
+			"message": "Unauthorized",
+		})
+		return
+	}
+
+	token := strings.Replace(bearerToken, "Bearer ", "", -1)
+
+	if token == "" {
+		ctx.AbortWithStatusJSON(401, gin.H{
+			"error": "Unauthorized",
+		})
+		return
+	}
+
+	if token != "123" {
+		ctx.AbortWithStatusJSON(401, gin.H{
+			"error": "Token is not valid",
+		})
+		return
+	}
+	ctx.Next()
+}
+
+func TokenMiddleware(ctx *gin.Context) {
 	token := ctx.GetHeader("X-Token")
 	if token == "" {
 		ctx.AbortWithStatusJSON(401, gin.H{
